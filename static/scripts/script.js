15db48f3606c34b0721c864cc9ac2498c2297b5d
asdfghjkl494 <justinekipchumba8@gmail.com>
2024-10-11 11:34:39 +0300

    Create script.js

---
+++ static/scripts/script.js
@@ -0,0 +1,12 @@
+document.querySelector('#fb').addEventListener('click',()=>{
+    window.location.href = '/facebook';
+});
+
+document.querySelector('#x').addEventListener('click',()=>{
+    window.location.href = '/twitter';
+});
+
+document.querySelector('#ig').addEventListener('click',()=>{
+    window.location.href = '/instagram';
+});
+
